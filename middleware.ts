8e4ca5d86566b95f35ff649a5e5a54c6625d7b2f
boranaybike <aybikeboran7140@gmail.com>
2024-06-13 14:44:47 +0300

    Add Role Permissions

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,47 @@
-import type { NextRequest } from 'next/server'
- 
-export function middleware(request: NextRequest) {
-  const currentUser = request.cookies.get('currentUser')?.value
- 
-  if (currentUser && !request.nextUrl.pathname.startsWith('/card')) {
-    return Response.redirect(new URL('/card', request.url))
-  }
- 
-  if (!currentUser && !request.nextUrl.pathname.startsWith('/')) {
-    return Response.redirect(new URL('/', request.url))
+import { NextResponse } from 'next/server'
+import { jwtVerify } from 'jose'
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET)
+
+async function verifyJWT(token:any) {
+  try {
+    const { payload } = await jwtVerify(token, secret);
+    return payload;
+  } catch (error) {
+    return null;
   }
 }
- 
+
+export async function middleware(request:any) {
+  const token = request.cookies.get('token')?.value;
+
+  const payload = token ? await verifyJWT(token) : null;
+
+   if (!payload) {
+    if (request.nextUrl.pathname !== '/') {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    return NextResponse.next();
+  }
+
+  request.user = {
+      id: payload.user_id,
+      role: payload.role 
+    };
+  
+  const pathname= request.nextUrl.pathname
+  if (pathname === '/menu/new' && payload.role === 'employee') {
+    return NextResponse.redirect(new URL('/403', request.url));
+  }
+
+  if ( pathname === '/movements' && pathname === '/booking' && pathname === '/movements' && payload.role !== 'admin') {
+    return NextResponse.redirect(new URL('/403', request.url))
+  }
+
+  return NextResponse.next();
+}
+
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/menu', '/', '/booking', '/movements','/menu/new']
 }
+
